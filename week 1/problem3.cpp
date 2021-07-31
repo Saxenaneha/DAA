21135831f00d7e91e5e5fe2bbb54c5e4f89700cf
Neha Saxena <87272909+Saxenaneha@users.noreply.github.com>
2021-07-31 21:45:09 +0530

    Update problem3.cpp

--- week 1/problem3.cpp
+++ week 1/problem3.cpp
@@ -1,1 +1,36 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        int c=0;
+        int key;
+        cin>>key;
+        bool x=true;
+        for(int i=0;i<n;i++)
+        {
+            c++;
+            if(a[i]==key)
+            {
+                x=false;
+                cout<<"Present"<<" "<<c<<endl;
+                break;
+            }
+        }
+        if(x)
+        {
+            cout<<"Not Present"<<" "<<c<<endl; 
+        }
+    }
+    return 0;
+}
