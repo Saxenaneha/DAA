a2d10defc41e417934ff46d51d6d3a029e4729a4
Neha Saxena <87272909+Saxenaneha@users.noreply.github.com>
2021-08-09 23:23:39 +0530

    Update problem2.cpp

--- week 3/problem2.cpp
+++ week 3/problem2.cpp
@@ -1,1 +1,37 @@
+#include <iostream.h>
+using namespace std;
 
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		int swaps=n-1;
+		int comparison=0;
+		for(int i=0;i<n-1;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				comparison++;
+				if(a[j]<a[i])
+				{
+					swap(a[i],a[j]);
+				}
+			}
+		}
+			for(int i=0;i<n;i++)
+		{
+			cout<<a[i]<<" ";
+		}
+		cout<<endl<<"comparisions = "<<comparison<<endl<<"swaps = "<<swaps<<endl;
+	}
+	return 0;
+}
