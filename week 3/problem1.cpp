46f178c5c1082417e419e4affdf28fdfcccc1691
Neha Saxena <87272909+Saxenaneha@users.noreply.github.com>
2021-08-09 23:13:48 +0530

    Update problem1.cpp

--- week 3/problem1.cpp
+++ week 3/problem1.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		int shift=0;
+		int comparisons=0;
+		for(int i=1;i<n;i++)
+		{
+			int current=a[i];
+			int j=i-1;
+			shift++;
+			while(a[j]>current and j>=0)
+			{
+				shift++;
+				comparisons++;
+				a[j+1]=a[j];
+				j--;
+			}
+			a[j+1]=current;
+		}
+		for(int i=0;i<n;i++)
+		{
+			cout<<a[i]<<" ";
+		}
+		cout<<endl<<"comparisions are = "<<comparisons<<endl<<"shifts  are= "<<shift<<endl;
+	}
+	return 0;
+}
